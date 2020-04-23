d44cbd33eacc08a25b2e7aa6f0a4ca4e44e1eb6b
tuanhuy210899 <63381899+tuanhuy210899@users.noreply.github.com>
2020-04-23 11:50:17 +0700

    Update api.php

--- api.php
+++ api.php
@@ -63,22 +63,21 @@ class api extends restful_api {
     {
         if(empty($params[0])||empty($params[1])||empty($params[2])||!empty($params[3]))
         {
-            return array("status" => false, "data" => array());
+           return array("status" => false, "data" => array());
         }
         else
         {
             $d=(double)$params[0];
             $m=(double)$params[1];
             $y=(double)$params[2];
-            if ($d > 31 || $m >12) 
-            {
-                return $data="Nhap ngay/thang khong dung";
+            if ($d > 31 || $m >12) {
+            return $data = "Nhap ngay/thang khong dung";
 
             } 
             elseif (($d == 31 && $m == 2)||($d == 30 && $m == 2)||($d == 31 && $m == 4)||($d == 31 && $m == 6)||($d == 31 && $m == 9)||($d == 31 && $m == 11)) 
             {
-                return $data="Nhap ngay khong dung voi thang";
-            }            
+               return $data = "Nhap ngay khong dung voi thang";
+            }
             elseif (($d == 31 && $m == 1)||($d == 31 && $m == 3)||($d == 31 && $m == 5)||($d == 31 && $m == 7)|| ($d == 31 && $m == 8)||($d == 31 && $m == 10)||($d == 31 && $m == 12)||($d == 28 && $m == 2)||($d == 30 && $m == 4)||($d == 30 && $m == 6)||($d == 30 && $m == 9)||($d == 30 && $m == 11)) 
             {
                if($m ==12)
@@ -86,15 +85,14 @@ class api extends restful_api {
                    $ngay = 1;
                    $thang =1;
                    $nam = $y +1;
-                   return $data="Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
+                   return $data= "ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
                }
                 else 
                 {
                     $ngay = 1;
                     $thang =$m +1;
                     $nam = $y;
-                    return $data="Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
-                    
+                   return $data= "Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
                 }                
             }
             else
@@ -102,7 +100,7 @@ class api extends restful_api {
                     $ngay = $d+1;
                     $thang = $m;
                     $nam = $y;
-                    return $data="Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
+                    return $data= "Ngay tiep theo la:ngay:".$ngay.",thang: ".$thang.", nam:".$nam."";
             }
         }
     }
