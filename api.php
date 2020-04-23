41d693588e6aa62c17a8db9049a72def1f3b6c64
tuanhuy210899 <63381899+tuanhuy210899@users.noreply.github.com>
2020-04-23 11:03:55 +0700

    Update api.php

--- api.php
+++ api.php
@@ -61,27 +61,50 @@ class api extends restful_api {
 
     function getyear($params)
     {
-        if(empty($params[0])||!empty($params[1]))
-        {
-            $data = array("status" => false, "data" => array());
-            return $data;
-        }elseif((double)$params[0]-(int)$params[0]!=0||!(int)$params[0]>0)
+        if(empty($params[0])||empty($params[1])||empty($params[2])||!empty($params[3]))
         {
             $data = array("status" => false, "data" => array());
             return $data;
         }
         else
         {
-            $x = (int)$params[0];
-            if ($x % 400 == 0 || $x % 4 == 0 && $x % 100 != 0) {
-                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " la nam nhuan"));
+            $d=(double)$params[0];
+            $m=(double)$params[1];
+            $y=(double)$params[2];
+            if ($d > 31 || $m >12) 
+            {
+                return array("status" => false, "data" => array());
 
-            } else {
-                $data = array("status" => true, "data" => array("result" => "Nam " . $x . " khong phai la nam nhuan"));
+            } 
+            elseif (($d == 31 && $m == 2)||($d == 30 && $m == 2)||($d == 31 && $m == 4)||($d == 31 && $m == 6)||($d == 31 && $m == 9)||($d == 31 && $m == 11)) 
+            {
+               return array("status" => false, "data" => array());
             }
-            return $data;
+            elseif (($d == 31 && $m == 1)||($d == 31 && $m == 3)||($d == 31 && $m == 5)||($d == 31 && $m == 7)|| ($d == 31 && $m == 8)||($d == 31 && $m == 10)||($d == 31 && $m == 12)||($d == 28 && $m == 2)||($d == 30 && $m == 4)||($d == 30 && $m == 6)||($d == 30 && $m == 9)||($d == 30 && $m == 11)) 
+            {
+               if($m ==12)
+               {
+                   $ngay = 1;
+                   $thang =1;
+                   $nam = $y +1;
+                   return array("status" => true,"data" => array("ngay"=>$ngay,"thang"=>$thang,"nam"=>$nam,"result"=>$data));
+               }
+                else 
+                {
+                    $ngay = 1;
+                    $thang =$m +1;
+                    $nam = $y;
+                    return array("status" => true,"data" => array("ngay"=>$ngay,"thang"=>$thang,"nam"=>$nam,"result"=>$data));
+                }                
+            }
+            else
+            {
+                    $ngay = $d+1;
+                    $thang = $m;
+                    $nam = $y;
+                    return array("status" => true,"data" => array("ngay"=>$ngay,"thang"=>$thang,"nam"=>$nam,"result"=>$data));
+            }
         }
     }
-}
 
 $user_api = new api();
